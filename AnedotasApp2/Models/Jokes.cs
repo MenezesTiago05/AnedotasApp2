110809be5245f23679d3626732137950b85bcf4d
21 <21@E306PC04>
2023-11-16 10:50:59 +0000

    Adicionar arquivos de projeto.

---
+++ AnedotasApp2/Models/Jokes.cs
@@ -0,0 +1,19 @@
+﻿namespace AnedotasApp2.Models
+{
+    public class Jokes
+    {
+
+        public int Id { get; set; }
+        public string JokePergunta { get; set; }
+        public string  JokeResposta { get; set; }
+
+        public Jokes()
+        {
+            Id = 0;
+            JokePergunta = String .Empty;
+            JokeResposta = String .Empty;
+
+        }
+
+    }
+}
